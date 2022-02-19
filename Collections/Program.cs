5decaeefa1057ef4a6ee9e6e6dadb220e0734b03
Alperen Alkan <alperen.alkann1@gmail.com>
2022-02-19 19:27:54 +0300

    19.02.2022

---
+++ Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] {"Aysema","Emin","Eslem","Metehan" };
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+
+            //names = new string[5];
+            //names[4] = "Alperen";
+            //Console.WriteLine(names[4]);
+            //Console.WriteLine(names[0]);
+
+            List<string> names2 = new List<string> { "Aysema", "Emin", "Eslem", "Metehan" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+            names2.Add("Alperen");
+            Console.WriteLine(names2[4]);
+            Console.WriteLine(names2[0]);
+            
+
+        }
+    }
+}
